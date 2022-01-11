24a8cf960b36648e563eec12d2200c58d8f15f89
tommytyc <tommytyc.cs06@nctu.edu.tw>
2022-01-11 21:19:04 +0800

    add imbalance dataset resampling, electra can reach 0.5704

--- roberta/dataset.py
+++ roberta/dataset.py
@@ -10,10 +10,11 @@ class DepressDataset(Dataset):
         self.mode = mode
         df = pd.read_csv(file_path, sep='\t')
         dic = {'not depression': 0, 'moderate': 1, 'severe': 2}
-        df['Label'] = df['Label'].map(dic)
+        df['label'] = df['label'].map(dic)
+        self.labels = df['label'].tolist()
         self.data = {}
         for idx, row in df.iterrows():
-            self.data[idx] = (row['Text_data'], row['Label'])
+            self.data[idx] = (row['Text_data'], row['label'])
         
     def __len__(self):
         return len(self.data)
@@ -21,3 +22,6 @@ class DepressDataset(Dataset):
     def __getitem__(self, idx):
         text, label = self.data[idx]
         return (text, torch.tensor(label, dtype=torch.long))
+
+    def get_labels(self):
+        return self.labels
--- roberta/main.py
+++ roberta/main.py
@@ -1,31 +1,44 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification,\
                          AdamW, get_scheduler  
-from datasets import Dataset, Features, ClassLabel, Value, features
 from dataset import DepressDataset
-from sklearn.metrics import f1_score
+from sklearn.metrics import precision_recall_fscore_support
 import pandas as pd
 from tqdm import tqdm
 import torch
 import torch.nn as nn
+from torch.optim import RAdam
 from torch.utils.data import DataLoader
+from torchsampler import ImbalancedDatasetSampler
 import wandb
+import sys
 
-MODEL = 'cardiffnlp/twitter-roberta-base-sentiment'
-MODEL_NAME = 'twitter-roberta-base-sentiment'
+MODEL = {
+    "roberta":{
+        "pretrain": "cardiffnlp/twitter-roberta-base-sentiment",
+        "name": "twitter-roberta-base-sentiment"
+    },
+    "electra":{
+        "pretrain": "google/electra-base-discriminator",
+        "name": "electra-base-discriminator"
+    }
+}
 EPOCHS = 30
 LR = 2e-5
 BATCH_SIZE = 4
 SEED = 17
+WARM_UP = 5
 
-wandb.init(project="depression-challenge", entity="nycu_adsl_depression_ycw", tags=["roberta"])
-wandb.config = {
-    "MODEL": MODEL,
-    "MODEL_NAME": MODEL_NAME,
-    "EPOCHS": EPOCHS,
-    "LR": LR,
-    "BATCH_SIZE": BATCH_SIZE,
-    "SEED": SEED
-}
+def set_wandb(model_type):
+    wandb.init(project="depression-challenge", entity="nycu_adsl_depression_ycw", tags=[model_type])
+    wandb.config = {
+        "MODEL": MODEL[model_type]["pretrain"],
+        "MODEL_NAME": MODEL[model_type]["name"],
+        "EPOCHS": EPOCHS,
+        "LR": LR,
+        "BATCH_SIZE": BATCH_SIZE,
+        "SEED": SEED,
+        "WARM_UP": WARM_UP
+    }
 
 def set_seed():
     torch.manual_seed(SEED)
@@ -35,17 +48,22 @@ def set_seed():
 def prepare_data(train_path, dev_path):
     train_data = DepressDataset(train_path, mode='train')
     dev_data = DepressDataset(dev_path, mode='test')
-    train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
+    # train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
+    train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE, sampler=ImbalancedDatasetSampler(train_data))
     dev_dataloader = DataLoader(dev_data, batch_size=1, shuffle=False)
     return train_dataloader, dev_dataloader
 
-def train(train_path, dev_path):
+def train(model_type, train_path, dev_path):
+    set_wandb(model_type)
+    set_seed()
     train_dataloader, dev_dataloader = prepare_data(train_path, dev_path)
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    model = AutoModelForSequenceClassification.from_pretrained(MODEL, num_labels=3).to(device)
-    tokenizer = AutoTokenizer.from_pretrained(MODEL)
+    # device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    device = torch.device("cpu")
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL[model_type]["pretrain"], num_labels=3).to(device)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL[model_type]["pretrain"])
     optimizer = AdamW(model.parameters(), lr=LR)
-    scheduler = get_scheduler("linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=len(train_dataloader)*EPOCHS)
+    # optimizer = RAdam(model.parameters(), lr=LR)
+    scheduler = get_scheduler("linear", optimizer=optimizer, num_warmup_steps=WARM_UP, num_training_steps=len(train_dataloader)*EPOCHS)
     criterion = nn.CrossEntropyLoss() 
     # check trained parameters
     print("Parameters to train:", sum(p.numel() for p in model.parameters() if p.requires_grad))
@@ -53,15 +71,17 @@ def train(train_path, dev_path):
     best_f1 = 0
     for epoch in tqdm(range(EPOCHS)):
         model.train()
+        total_loss = 0
         for data in train_dataloader:
+            optimizer.zero_grad()
             text, label = list(data[0]), data[1].to(device)
             input_text = tokenizer(text, padding=True, truncation=True, max_length=512, return_tensors="pt").to(device)
             outputs = model(**input_text).logits
             loss = criterion(outputs, label)
+            total_loss += loss.item()
             loss.backward()
             optimizer.step()
-            scheduler.step()
-            optimizer.zero_grad()
+        scheduler.step()
     
         model.eval()
         pred = []
@@ -73,13 +93,20 @@ def train(train_path, dev_path):
                 outputs = model(**input_text).logits
             pred.append(torch.argmax(outputs, dim=-1).cpu().numpy())
             labels.append(label.cpu().numpy())
-        f1 = round(f1_score(labels, pred, average='macro'), 4)
-        print(f"Epoch: {epoch}, F1 score: {f1}")
-        wandb.log({"Epoch": epoch, "F1": f1})
+        precision, recall, f1, support = precision_recall_fscore_support(labels, pred, average='macro', zero_division=1)
+        precision = round(precision, 4)
+        recall = round(recall, 4)
+        f1 = round(f1, 4)
+        avg_loss = round(total_loss/len(train_dataloader), 4)
+        print(f"Epoch: {epoch}, F1 score: {f1}, Loss: {avg_loss}")
+        wandb.log({"epoch": epoch, "f1": f1, "train loss": avg_loss, "precision": precision, "recall": recall, "support": support})
         if f1 > best_f1:
             best_f1 = f1
-            torch.save(model.state_dict(), f"../model/{MODEL_NAME}_{f1}.pt")
+            torch.save(model.state_dict(), f"../model/{MODEL[model_type]['name']}_{f1}.pt")
 
 
 if __name__ == '__main__':
-    train(train_path='../data/train.tsv', dev_path='../data/dev_with_labels.tsv')
+    model_type = sys.argv[1]
+    if model_type not in MODEL.keys():
+        raise ValueError(f"{model_type} is not a valid model type [roberta, electra]")
+    train(model_type, train_path='../data/train.tsv', dev_path='../data/dev_with_labels.tsv')
