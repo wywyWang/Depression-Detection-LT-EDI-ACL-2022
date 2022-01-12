a286130b65e810a7dac19a9c2d99ccc2132a5c07
tommytyc <tommytyc.cs06@nctu.edu.tw>
2022-01-12 16:37:37 +0800

    add ensemble part

---
+++ roberta/ensemble.py
@@ -0,0 +1,77 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification,\
+                         AdamW, get_scheduler  
+from dataset import DepressDataset
+from sklearn.metrics import precision_recall_fscore_support
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+from torch.optim import RAdam
+from torch.utils.data import DataLoader
+import wandb
+
+MODEL1 = 'cardiffnlp/twitter-roberta-base-sentiment'
+# MODEL1_NAME = 'twitter-roberta-base-sentiment_0.5478'
+MODEL1_NAME = 'twitter-roberta-base-sentiment_0.5579'
+MODEL2 = 'google/electra-base-discriminator'
+# MODEL2_NAME = 'electra-base-discriminator_0.5485'
+MODEL2_NAME = 'electra-base-discriminator_0.5704'
+MODEL3 = 'microsoft/deberta-v3-base'
+MODEL3_NAME = 'deberta-v3-base_0.5579'
+BATCH_SIZE = 4
+SEED = 17
+
+def set_seed():
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+    torch.cuda.manual_seed_all(SEED)
+
+def prepare_data(dev_path):
+    dev_data = DepressDataset(dev_path, mode='test')
+    dev_dataloader = DataLoader(dev_data, batch_size=1, shuffle=False)
+    return dev_dataloader
+
+def test(dev_path):
+    set_seed()
+    dev_dataloader = prepare_data(dev_path)
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    model1 = AutoModelForSequenceClassification.from_pretrained(MODEL1, num_labels=3).to(device)
+    model2 = AutoModelForSequenceClassification.from_pretrained(MODEL2, num_labels=3).to(device)
+    model3 = AutoModelForSequenceClassification.from_pretrained(MODEL3, num_labels=3).to(device)
+    tokenizer1 = AutoTokenizer.from_pretrained(MODEL1)
+    tokenizer2 = AutoTokenizer.from_pretrained(MODEL2)
+    tokenizer3 = AutoTokenizer.from_pretrained(MODEL3)
+    # check trained parameters
+
+    model1.load_state_dict(torch.load(f"../model/{MODEL1_NAME}.pt"))
+    model2.load_state_dict(torch.load(f"../model/{MODEL2_NAME}.pt"))
+    model3.load_state_dict(torch.load(f"../model/{MODEL3_NAME}.pt"))
+
+    model1.eval()
+    model2.eval()
+    model3.eval()
+    pred = []
+    labels = []
+    for data in dev_dataloader:
+        text, label = list(data[0]), data[1].to(device)
+        input_text1 = tokenizer1(text, padding=True, truncation=True, max_length=512, return_tensors="pt").to(device)
+        input_text2 = tokenizer2(text, padding=True, truncation=True, max_length=512, return_tensors="pt").to(device)
+        input_text3 = tokenizer3(text, padding=True, truncation=True, max_length=512, return_tensors="pt").to(device)
+        with torch.no_grad():
+            outputs1 = model1(**input_text1).logits
+            outputs2 = model2(**input_text2).logits
+            outputs3 = model3(**input_text3).logits
+            tmp = torch.add(torch.add(outputs1, outputs2), outputs3)
+        pred.append(torch.argmax(tmp, dim=-1).cpu().numpy())
+        labels.append(label.cpu().numpy())
+    
+    precision, recall, f1, support = precision_recall_fscore_support(labels, pred, average='macro', zero_division=1)
+    precision = round(precision, 4)
+    recall = round(recall, 4)
+    f1 = round(f1, 4)
+    print(f"F1 score: {f1}, Precision: {precision}, Recall: {recall}")
+
+
+if __name__ == '__main__':
+    test(dev_path='../data/dev_with_labels.tsv')
