882e0011ac9233e6544ff346a809d5c583867071
tommytyc <tommytyc.cs06@nctu.edu.tw>
2022-01-26 13:13:55 +0800

    add test ensemble

--- roberta/dataset.py
+++ roberta/dataset.py
@@ -10,18 +10,27 @@ class DepressDataset(Dataset):
         self.mode = mode
         df = pd.read_csv(file_path, sep='\t')
         dic = {'not depression': 0, 'moderate': 1, 'severe': 2}
-        df['label'] = df['label'].map(dic)
-        self.labels = df['label'].tolist()
-        self.data = {}
-        for idx, row in df.iterrows():
-            self.data[idx] = (row['Text_data'], row['label'])
+        if mode != 'test':
+            df['label'] = df['label'].map(dic)
+            self.labels = df['label'].tolist()
+            self.data = {}
+            for idx, row in df.iterrows():
+                self.data[idx] = (row['Text_data'], row['label'])
+        else:
+            self.data = {}
+            for idx, row in df.iterrows():
+                self.data[idx] = (row['PID'], row['Text_data'])
         
     def __len__(self):
         return len(self.data)
     
     def __getitem__(self, idx):
-        text, label = self.data[idx]
-        return (text, torch.tensor(label, dtype=torch.long))
+        if self.mode != 'test':
+            text, label = self.data[idx]
+            return (text, torch.tensor(label, dtype=torch.long))
+        else:
+            pid, text = self.data[idx]
+            return (pid, text)
 
     def get_labels(self):
         return self.labels
--- roberta/ensemble.py
+++ roberta/ensemble.py
@@ -7,34 +7,32 @@ import numpy as np
 from tqdm import tqdm
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.optim import RAdam
 from torch.utils.data import DataLoader
-import wandb
+import argparse
 
 MODEL1 = 'cardiffnlp/twitter-roberta-base-sentiment'
-# MODEL1_NAME = 'twitter-roberta-base-sentiment_0.5478'
-MODEL1_NAME = 'twitter-roberta-base-sentiment_0.5579'
+MODEL1_NAME = 'twitter-roberta-base-sentiment_0.5686'
 MODEL2 = 'google/electra-base-discriminator'
-# MODEL2_NAME = 'electra-base-discriminator_0.5485'
 MODEL2_NAME = 'electra-base-discriminator_0.5704'
 MODEL3 = 'microsoft/deberta-v3-base'
 MODEL3_NAME = 'deberta-v3-base_0.5579'
-BATCH_SIZE = 4
-SEED = 17
 
-def set_seed():
-    torch.manual_seed(SEED)
-    torch.cuda.manual_seed(SEED)
-    torch.cuda.manual_seed_all(SEED)
+def set_arg():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', type=str, default='../data/dev_with_labels.tsv')
+    parser.add_argument('--mode', type=str, default='dev')
+    return parser.parse_args()
 
-def prepare_data(dev_path):
-    dev_data = DepressDataset(dev_path, mode='test')
-    dev_dataloader = DataLoader(dev_data, batch_size=1, shuffle=False)
-    return dev_dataloader
+def prepare_data(path, mode='dev'):
+    data = DepressDataset(path, mode)
+    dataloader = DataLoader(data, batch_size=1, shuffle=False)
+    return dataloader
 
-def test(dev_path):
-    set_seed()
-    dev_dataloader = prepare_data(dev_path)
+def ensemble(args):
+    path, mode = args.path, args.mode
+    dataloader = prepare_data(path, mode)
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model1 = AutoModelForSequenceClassification.from_pretrained(MODEL1, num_labels=3).to(device)
     model2 = AutoModelForSequenceClassification.from_pretrained(MODEL2, num_labels=3).to(device)
@@ -52,9 +50,12 @@ def test(dev_path):
     model2.eval()
     model3.eval()
     pred = []
-    labels = []
-    for data in dev_dataloader:
-        text, label = list(data[0]), data[1].to(device)
+    pids, labels = [], []
+    for data in dataloader:
+        if mode == 'dev':
+            text, label = list(data[0]), data[1]
+        else:
+            pid, text = data[0][0], list(data[1])
         input_text1 = tokenizer1(text, padding=True, truncation=True, max_length=512, return_tensors="pt").to(device)
         input_text2 = tokenizer2(text, padding=True, truncation=True, max_length=512, return_tensors="pt").to(device)
         input_text3 = tokenizer3(text, padding=True, truncation=True, max_length=512, return_tensors="pt").to(device)
@@ -62,16 +63,25 @@ def test(dev_path):
             outputs1 = model1(**input_text1).logits
             outputs2 = model2(**input_text2).logits
             outputs3 = model3(**input_text3).logits
-            tmp = torch.add(torch.add(outputs1, outputs2), outputs3)
-        pred.append(torch.argmax(tmp, dim=-1).cpu().numpy())
-        labels.append(label.cpu().numpy())
+            tmp = torch.add(torch.add(outputs1, outputs2), outputs3) / 3
+        if mode == 'dev':
+            pred.append(torch.argmax(F.softmax(tmp, dim=1), dim=-1).cpu().numpy())
+            labels.append(label)
+        else:
+            pred.append(F.softmax(tmp, dim=1).cpu().numpy())
+            pids.append(pid)
     
-    precision, recall, f1, support = precision_recall_fscore_support(labels, pred, average='macro', zero_division=1)
-    precision = round(precision, 4)
-    recall = round(recall, 4)
-    f1 = round(f1, 4)
-    print(f"F1 score: {f1}, Precision: {precision}, Recall: {recall}")
-
+    if mode == 'dev':
+        precision, recall, f1, support = precision_recall_fscore_support(labels, pred, average='macro', zero_division=1)
+        precision = round(precision, 4)
+        recall = round(recall, 4)
+        f1 = round(f1, 4)
+        print(f"F1 score: {f1}, Precision: {precision}, Recall: {recall}")
+    else:
+        pred = np.array(pred).T
+        prob_df = pd.DataFrame({'PID': pids, 'not depression': pred[0][0], 'moderate': pred[1][0], 'severe': pred[2][0]})
+        prob_df.to_csv(f"../result/ensemble.csv", index=False)
 
 if __name__ == '__main__':
-    test(dev_path='../data/dev_with_labels.tsv')
+    args = set_arg()
+    ensemble(args)
